2b0805910ad53247bb3a1bd7ef300e9bed059477
Sameer Sayyed <sayyedsameer17995@gmail.com>
2026-08-22 15:00:12 +0530

    Add files via upload

---
+++ Mini_Calculator.py
@@ -0,0 +1,53 @@
+Pin = 1234
+attempts = 0
+
+while attempts < 3:
+    password = int(input("Enter your Pin: "))
+
+    if password == Pin:
+        print("Login Successfully")
+        print("=" * 40)
+        print("\tMy name is Mini Calculator")
+        print("=" * 40)
+        break
+
+    attempts += 1
+    print("Wrong Pin\n")
+
+else:
+    print("You are Blocked")
+
+# Calculator only starts if PIN is correct
+if attempts < 3:
+    while True:
+        num1 = float(input("Enter your Number: "))
+        operator = input("Enter Your Operator (+, -, *, /): ")
+        num2 = float(input("Enter your Number: "))
+
+        if operator == '+':
+            print("Here is your Addition result")
+            print(num1 + num2)
+            print("Thanks For Using Calculator\n")
+
+        elif operator == '-':
+            print("Here is your Subtraction result")
+            print(num1 - num2)
+            print("Thanks For Using Calculator\n")
+
+        elif operator == '*':
+            print("Here is your Multiplication result")
+            print(num1 * num2)
+            print("Thanks For Using Calculator\n")
+
+        elif operator == '/':
+            if num2 == 0:
+                print("Not Divisible by Zero\n")
+            else:
+                print("Here is your Division result")
+                print(num1 / num2)
+                print("Thanks For Using Calculator\n")
+
+        else:
+            print("Wrong Operator")
+            print("Thanks For Using Calculator\n")
+            break
